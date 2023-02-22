6b045f7b82b0cd56701a47c2694e5a20c96b493f
unknown <you@example.com>
2023-02-22 22:07:53 +0100

    firstChange

--- src/firstGitSenkronization/UseGitHub.java
+++ src/firstGitSenkronization/UseGitHub.java
@@ -3,5 +3,7 @@ package firstGitSenkronization;
 public class UseGitHub {
     public static void main(String[] args) {
         System.out.println("Hello Hackers");
+        String name="Ahmed";
+        System.out.println(name+ " came.");
     }
 }
